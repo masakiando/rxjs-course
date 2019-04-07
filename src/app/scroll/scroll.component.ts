76215d849bdfaf907fd7871af07e8ecf1adef8a5
Your Name <you@example.com>
2019-04-07 14:34:35 +0700

    s

--- src/app/scroll/scroll.component.ts
+++ src/app/scroll/scroll.component.ts
@@ -1,40 +1,53 @@
 import { Component, OnInit } from '@angular/core';
-import { merge, interval, concat, timer, fromEvent, Observable, noop, of, forkJoin, Subject, BehaviorSubject, range, from} from 'rxjs';
-import {distinct, filter, map, debounceTime, tap, flatMap} from 'rxjs/operators';
-import {HttpClient} from '@angular/common/http';
+import {
+  merge,
+  interval,
+  concat,
+  timer,
+  fromEvent,
+  Observable,
+  noop,
+  of,
+  forkJoin,
+  Subject,
+  BehaviorSubject,
+  range,
+  from,
+} from 'rxjs';
+import { distinct, filter, map, debounceTime, tap, flatMap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 import * as _ from 'lodash';
 
 @Component({
-    selector: 'app-infinite-scroll-list',
-    template: `
+  selector: 'app-infinite-scroll-list',
+  template: `
     <table>
       <tbody>
-        <tr *ngFor="let item of itemResults$|async" [style.height]="itemHeight + 'px'">
-          <td>{{item.name}}</td>
+        <tr *ngFor="let item of (itemResults$ | async)" [style.height]="itemHeight + 'px'">
+          <td>{{ item.name }}</td>
         </tr>
       </tbody>
-     </table>
-    `
+    </table>
+  `,
 })
 export class ScrollComponent implements OnInit {
-    private cache = [];
-    private pageByManual$ = new BehaviorSubject(1);
-    private itemsHeight = 40;
-    private numberOfItems = 10;
-    private pageByScroll$ = fromEvent(window, 'scroll')
+  private cache = [];
+  private pageByManual$ = new BehaviorSubject(1);
+  private itemsHeight = 40;
+  private numberOfItems = 10;
+  private pageByScroll$ = fromEvent(window, 'scroll')
     .pipe(
-     map( () => window.scrollY),
-     tap(v => console.log(v)),
-     filter(current => current >= document.body.clientHeight - window.innerHeight),
-     debounceTime(200),
-     map(y => Math.ceil((y + window.innerHeight) / (this.itemsHeight * this.numberOfItems)))
-    ).subscribe(data => console.log(data));
+      map(() => window.scrollY),
+      tap(v => console.log(v)),
+      filter(current => current >= document.body.clientHeight - window.innerHeight),
+      debounceTime(200),
+      map(y => Math.ceil((y + window.innerHeight) / (this.itemsHeight * this.numberOfItems)))
+    )
+    .subscribe(data => console.log(data));
 
-    // console.log(document.body.clientHeigh);
-    
-    constructor() { }
+  // console.log(document.body.clientHeigh);
 
-    ngOnInit() {
-    }
+  constructor() {}
 
+  ngOnInit() {}
 }
