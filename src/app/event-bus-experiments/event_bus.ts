aed83bdc75d597dcfe21b3cbb462a60cca674642
Your Name <you@example.com>
2019-04-07 14:53:15 +0700

    EventBusにObserver、Subject実装

---
+++ src/app/event-bus-experiments/event_bus.ts
@@ -0,0 +1,19 @@
+export interface Observer {
+  notify(data: any);
+}
+
+interface Subject {
+  registerObserver(obs: Observer);
+  unregisterObserver(obs: Observer);
+  notifyObserver(data: any);
+}
+
+class EventBus implements Subject {
+  private observers: Observer[] = [];
+
+  registerObserver(obs: Observer) {
+      this.observers.push(obs);
+  }
+  unregisterObserver(obs: Observer) {}
+  notifyObserver(data: any) {}
+}
