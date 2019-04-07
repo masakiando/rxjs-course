6814c405a2ff41e16ac1a506a3b6091be40030e2
Your Name <you@example.com>
2019-04-07 16:52:47 +0700

    17.グローバルイベントバスの改善 - さまざまな種類のアプリケーションイベントへのサポートの追加

--- src/app/event-bus-experiments/event-bus-experiments.component.ts
+++ src/app/event-bus-experiments/event-bus-experiments.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { globalEventBus } from './event_bus';
+import { EventBus_Lessons } from '../model/EventBus_LessonsTable';
 @Component({
   selector: 'event-bus-experiments',
   templateUrl: './event-bus-experiments.component.html',
-  styleUrls: ['./event-bus-experiments.component.scss']
+  styleUrls: ['./event-bus-experiments.component.scss'],
 })
 export class EventBusExperimentsComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    globalEventBus.notifyObserver(EventBus_Lessons);
   }
-
 }
--- src/app/event-bus-experiments/event_bus.ts
+++ src/app/event-bus-experiments/event_bus.ts
@@ -1,3 +1,4 @@
+import * as _ from 'lodash';
 export interface Observer {
   notify(data: any);
 }
@@ -12,8 +13,22 @@ class EventBus implements Subject {
   private observers: Observer[] = [];
 
   registerObserver(obs: Observer) {
-      this.observers.push(obs);
+    console.log('EventBus registerObserver');
+    this.observers.push(obs);
+    console.log(this.observers);
   }
-  unregisterObserver(obs: Observer) {}
-  notifyObserver(data: any) {}
+  unregisterObserver(obs: Observer) {
+    console.log('EventBus unregisterObserver');
+    _.remove(this.observers, el => el === obs);
+  }
+  notifyObserver(data: any) {
+    console.log('EventBus notifyObserver');
+    this.observers.forEach(obs => {
+      console.log(obs);
+      console.log(data);
+      obs.notify(data);
+    });
+  }
 }
+
+export const globalEventBus = new EventBus();
