a3cda048310db4e41157a1e48829036d695513d5
Your Name <you@example.com>
2019-04-12 20:57:05 +0700

    2

--- src/app/lessons-list/lessons-list.component.ts
+++ src/app/lessons-list/lessons-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Lesson } from '../model/LessonsTable';
 import { globalEventBus, Observer, LESSONS_LIST_AVAILABLE, ADD_NEW_LESSON } from '../event-bus-experiments/event_bus';
+import * as _ from 'lodash';
 @Component({
   selector: 'lessons-list',
   templateUrl: './lessons-list.component.html',
@@ -28,5 +29,10 @@ export class LessonsListComponent implements Observer {
 
   toggleLessonViewed(lesson: Lesson) {
     lesson.completed = !lesson.completed;
+  }
+
+  delete(deleted: Lesson) {
+    _.remove(this.lessons, lesson => lesson.id === deleted.id);
+    globalEventBus.notifyObserver(LESSONS_LIST_AVAILABLE, this.lessons);
   }
 }
