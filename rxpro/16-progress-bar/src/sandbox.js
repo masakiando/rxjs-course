46647cbf099e4778d758000c3a20d9918f0bdd49
Your Name <you@example.com>
2019-05-14 07:09:50 +0900

    d

---
+++ rxpro/16-progress-bar/src/sandbox.js
@@ -0,0 +1,64 @@
+import { updateDisplay } from "./utils";
+import { fromEvent } from "rxjs";
+import { map, tap, share } from "rxjs/operators";
+
+export default () => {
+  /** start coding */
+
+  const progressBar = document.getElementById("progress-bar");
+  const docElement = document.documentElement;
+
+  // function to update progress bar width on view
+  const updateProgressBar = percentage => {
+    const red = () => {
+      return "red";
+    };
+    const green = () => {
+      return "green";
+    };
+    const yellow = () => {
+      return "yellow";
+    };
+
+    function getColor(type) {
+      const colors = {
+        red: red,
+        green: green,
+        yellow: yellow
+      };
+      return colors[type]();
+    }
+    console.log(percentage);
+
+    progressBar.style.backgroundColor =
+      percentage < 70 ? getColor("yellow") : getColor("green");
+    progressBar.style.width = `${percentage}%`;
+  };
+
+  // observable that returns scroll (from top) on scroll events
+  const scroll$ = fromEvent(document, "scroll").pipe(
+    map(() => docElement.scrollTop),
+    tap(evt => console.log("[scroll]: ", evt))
+  );
+
+  // observable that returns the amount of page scroll progress
+  const scrollProgress$ = scroll$.pipe(
+    map(evt => {
+      const docHeight = docElement.scrollHeight - docElement.clientHeight;
+      return (evt / docHeight) * 100;
+    }),
+    share()
+  );
+
+  // subscribe to scroll progress to paint a progress bar
+  const subscription = scrollProgress$.subscribe(updateProgressBar);
+
+  // subscribe to display scroll progress percentage
+  const subscription2 = scrollProgress$.subscribe(val =>
+    updateDisplay(`${Math.floor(val)} %`)
+  );
+
+  /** end coding */
+};
+
+// https://www.codingame.com/playgrounds/24002/becoming-functional/functional-mindset
